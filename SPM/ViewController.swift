b8d5b448cbbbf6ac0fff7cb5761fd815434a1d5f
Noel C Perez <noelcarlos.perez@gmail.com>
2021-12-16 13:55:04 -0500

    Initial Commit

---
+++ SPM/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SPM
+//
+//  Created by Noel C Perez on 12/16/21.
+//
+
+import UIKit
+import Albums
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
